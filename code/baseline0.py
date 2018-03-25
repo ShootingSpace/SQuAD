5736596068c39b7629aaabb60dea31242dd57763
Cong <erdongerzong@qq.com>
2018-03-25 13:12:39 +0100

    -

--- code/baseline0.py
+++ code/baseline0.py
@@ -10,6 +10,7 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 from tensorflow.python.ops import variable_scope as vs
 from utils.evaluate import exact_match_score, f1_score
+from utils.util import *
 from model import Model, Encoder, Decoder
 from utils.result_saver import ResultSaver
 
